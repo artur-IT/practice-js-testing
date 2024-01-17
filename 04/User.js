412c8a86fe084d84368e537d7f30a1109304b7c1
Artur Matysiak <inz.matys@gmail.com>
2024-01-17 20:13:18 +0100

    add check test .login() - 1 and 2 done

--- 04/User.js
+++ 04/User.js
@@ -1,7 +1,20 @@
 export default class User {
-  constructor(email, password) {
-    email: email;
-    password: password;
-    user: user;
+  // class User {
+  constructor({ email, password }) {
+    this.email = email;
+    this.password = password;
   }
+  getEmail = () => {
+    const emailRegEx = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/g;
+    if (emailRegEx.test(this.email)) return this.email;
+    else throw new Error("Hmmmm");
+  };
+  getPassword = () => this.password;
+
+  login = () => {
+    const checkDomain = this.email.includes("devmentor.pl");
+    return checkDomain ? true : false;
+  };
 }
+// const test = new User({ email: "artur@evmentor.pl", password: "23456" });
+// console.log(test.login());
