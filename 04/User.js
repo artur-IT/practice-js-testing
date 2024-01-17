4c505ae7f6d07cebc7a24ade96d84789ddf5db41
Artur Matysiak <inz.matys@gmail.com>
2024-01-17 20:58:03 +0100

    all tests is green - task  done

--- 04/User.js
+++ 04/User.js
@@ -1,20 +1,22 @@
 export default class User {
-  // class User {
   constructor({ email, password }) {
     this.email = email;
     this.password = password;
+    this.getEmail();
+    this.getPassword();
   }
   getEmail = () => {
     const emailRegEx = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}/g;
     if (emailRegEx.test(this.email)) return this.email;
-    else throw new Error("Hmmmm");
+    else throw new Error("Wrong email");
   };
-  getPassword = () => this.password;
+  getPassword = () => {
+    if (this.password.length < 8) throw new Error("To short password");
+    else return this.password;
+  };
 
   login = () => {
     const checkDomain = this.email.includes("devmentor.pl");
     return checkDomain ? true : false;
   };
 }
-// const test = new User({ email: "artur@evmentor.pl", password: "23456" });
-// console.log(test.login());
